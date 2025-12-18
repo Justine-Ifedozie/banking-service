591c1c028ace239ca6d334b104e76f383995aa53
Justine Ifedozie <ifedoziennamdi445@gmail.com>
2025-12-18 22:07:49 +0100

    created new classes

--- src/app.js
+++ src/app.js
@@ -3,8 +3,11 @@ const cors = require('cors');
 
 const app = express();
 
+const authRoutes = require('./routes/auth.routes');
+
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoutes);
 
 app.get('/health', (_, res) => {
     res.json({status: 'OK', service: 'Banking Service', timestamp: new Date() });
---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,3 @@
+exports.register = async (req, res) => {
+    return res.status(201).json({message: 'Registration endpoint placeholder'});
+};
