dd2db43a033504f886d506a5abbcb6002f0c5667
Justine Ifedozie <ifedoziennamdi445@gmail.com>
2025-12-23 07:00:53 +0100

    new files

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 const authService = require('../services/auth.service');
-const { registerSchema } = require('../validators/auth.validator');
+const { registerSchema, loginSchema } = require('../validators/auth.validator');
 const AppError = require('../middleware/appError');
 
 exports.register = async (req, res, next) => {
@@ -18,6 +18,28 @@ exports.register = async (req, res, next) => {
                 email: user.email,
                 phone: user.phone
             }
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+exports.login = async (req, res, next) => {
+    try {
+        const { error, value } = loginSchema.validate(req.body);
+        if (error) {
+            throw new AppError(error.details[0].message, 400)
+        }
+
+        const { user, token } = await authService.login(value);
+
+        res.status(200).json({
+            message: 'Login successful',
+            token,
+            user: {
+                id: user.id,
+                email: user.email,
+            },
         });
     } catch (err) {
         next(err);
